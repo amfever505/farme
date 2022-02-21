cbc045eeb63c7f24f95f74fe6182bfcb762c65f0
amfever505 <amfeve505@gmail.com>
2022-02-22 00:38:10 +0900

    first

---
+++ 3D/model.js
@@ -0,0 +1,277 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/loaders/GLTFLoader.js';
+import * as modelData from './modelData.js';
+
+let model;
+
+async function init(url) {
+  const canvas = document.querySelector('#c');
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  const scene = new THREE.Scene();
+  // scene.background = new THREE.Color('white');
+  renderer.setClearColor(0xf9f9f9, 1);
+
+  const width = 640;
+  const height = 480;
+
+  renderer.setPixelRatio(1);
+  renderer.setSize(width, height);
+  // カメラ設定
+  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  camera.position.set(0, 0, 60);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  // ライティング
+  renderer.shadowMap.enabled = true;
+  const light = new THREE.AmbientLight(0xffffff, 1);
+  const spotlight = new THREE.SpotLight(0xffffff, 1, 200, 1, 1, 2);
+  spotlight.castShadow = true;
+  spotlight.position.set(50, 0, 30);
+  const leftlight = new THREE.SpotLight(0xffffff, 1, 200, 1, 1, 2);
+  leftlight.position.set(-50, 0, 30);
+
+  scene.add(spotlight, leftlight);
+  scene.add(light);
+  renderer.outputEncoding = THREE.GammaEncoding;
+
+  // モデルloader
+  const loader = new GLTFLoader();
+  model = await (() => {
+    return new Promise((resolve) => {
+      loader.load(
+        url,
+        (gltf) => {
+          resolve(gltf.scene);
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    });
+  })();
+
+  // カスタマイズ　箇所の指定など
+
+  // 全モデルのキャンバスはこうしなくちゃいけない
+  const Canvas = model.getObjectByName('canvas_平面').material;
+
+  let customizeObj = '';
+  let customizeMoyou = '';
+  let Picker = [];
+  let moyouResults = [];
+
+  // カラー変更のエリアを作る
+  MDL.object.forEach((item, index) => {
+    customizeObj += `${item.name} <input type="color" id="${item.name}" value="${item.color}" /><br>`;
+    Picker[index] = [item.name, item.value, '0x' + item.color.slice('1')];
+  });
+  $('#color-area').html(customizeObj);
+
+  // 模様変更のエリアを作る
+  if (MDL.moyou.length > 0) {
+    customizeMoyou = '模様：';
+    MDL.moyou.forEach((item, index) => {
+      customizeMoyou += `<br><label> <input type="checkbox" id="${item.value}" name="${index}" class="moyouOption" checked>${item.name}</label>`;
+      moyouResults.push(true);
+    });
+  }
+  $('#moyou-area').html(customizeMoyou);
+  $('.moyouOption').on('change', function () {
+    model.getObjectByName($(this)[0].id).material.visible = !model.getObjectByName($(this)[0].id).material.visible;
+
+    moyouResults[$(this)[0].name] = $(this).prop('checked');
+    console.log(moyouResults);
+  });
+  // カラーピッカーのイベントハンドラー
+
+  Picker.forEach((x) => {
+    $('#' + x[0]).on('change', function () {
+      let color =
+        '0x' +
+        $('#' + x[0])
+          .val()
+          .slice(1);
+      x[1].forEach((val) => model.getObjectByName(val).material.color.setHex(color));
+      x[2] = color;
+    });
+  });
+
+  model.traverse((object) => {
+    if (object.isMesh) {
+      object.material.transparent = true;
+    }
+  });
+  //位置調整
+  const box = new THREE.Box3().setFromObject(model);
+  const modelW = box.max.x - box.min.x;
+  const modelH = box.max.y - box.min.y;
+  // const modelD = box.max.z - box.min.z;
+
+  // texture変更
+  // var filechange = document.getElementById('file');
+  // filechange.addEventListener('change', textureSwitch, false);
+
+  var img = document.createElement('img');
+  img.src = '/001.jpg';
+
+  // テクスチャの更新（meshは作成済みのオブジェクト）
+  Canvas.side = THREE.doubleSide;
+  Canvas.map = new THREE.Texture(img);
+  Canvas.map.needsUpdate = true;
+
+  // 写真プレビューを切る
+  const imgPreview = $('#image-preview');
+  imgPreview.on('click', () => {
+    if (!imgPreview.prop('checked')) {
+      img.src = '/000.jpg';
+    } else {
+      img.src = '/001.jpg';
+    }
+    Canvas.map = new THREE.Texture(img);
+    Canvas.map.needsUpdate = true;
+  });
+
+  function textureSwitch(event) {
+    // 要素内イベントのキャンセル
+    event.preventDefault();
+
+    // ファイルを読み込む　→デフォルトにする
+    var file = event.target.files[0];
+
+    // FileReader オブジェクトの作成
+    var reader = new FileReader();
+
+    reader.onload = function (event) {
+      // 「img」でエレメントオブジェクト作成
+      var img = document.createElement('img');
+      img.src = event.target.result;
+
+      // テクスチャの更新（meshは作成済みのオブジェクト）
+      Canvas.side = THREE.DoubleSide;
+      Canvas.map = new THREE.Texture(img);
+      Canvas.map.needsUpdate = true;
+      console.log(Canvas.map);
+    };
+    // データURLとしてファイルを読み込む
+    reader.readAsDataURL(file);
+  }
+  // 位置設定
+  model.position.set(-modelW / 60, -modelH / 10, -25);
+
+  // const Tate = document.querySelector('#tate');
+  // const Yoko = document.querySelector('#Yoko');
+  let Direction = 'yoko';
+  $('#tate').on('click', () => {
+    Direction = 'tate';
+    directionChange(Direction);
+  });
+  $('#yoko').on('click', () => {
+    Direction = 'yoko';
+    directionChange(Direction);
+  });
+  function directionChange() {
+    if (Direction == 'tate') {
+      model.rotation.set(0, 0, Math.PI / 2);
+    } else {
+      model.rotation.set(0, 0, 0);
+    }
+  }
+
+  // console.log(modelW / camera.position.z);
+
+  model.scale.set(MDL.scale, MDL.scale, MDL.scale);
+  scene.add(model);
+  // いらないかも？
+  renderer.setAnimationLoop(tick);
+
+  // ar url作成
+  const arBtn = document.querySelector('#arBtn');
+
+  // let arLink = 'https://iwnb3.csb.app/?';
+  let arLink = 'https://farme-test.herokuapp.com/ar.html?'; //heroku test 環境
+
+  arBtn.addEventListener('click', (e) => {
+    // idとりあえず1でお試し
+
+    let Param = [{ id: MDL.id }, { size: modelNo.value }, { dir: Direction }, { moyou: moyouResults }];
+    Picker.forEach((x) => {
+      let obj = new Object();
+      obj[x[0]] = x[2];
+      Param.push(obj);
+    });
+
+    let urlParam = '';
+    Param.forEach((x) => {
+      urlParam += Object.keys(x) + '=' + Object.values(x) + '&';
+    });
+
+    arLink = arLink + urlParam;
+    console.log(arLink);
+    $(function () {
+      var qrtext = arLink;
+      var utf8qrtext = unescape(encodeURIComponent(qrtext));
+      $('#qr').html('');
+      $('#qr').qrcode({ width: 160, height: 160, text: utf8qrtext });
+    });
+
+    arLink = 'https://farme-test.herokuapp.com/ar.html?';
+  });
+
+  // アニメーション関連
+  function tick() {
+    controls.update();
+
+    // if (model) {
+    //   model.rotation.x += 0;
+    // }
+    renderer.render(scene, camera);
+  }
+
+  // Mesh であるやつを取得
+  console.log(model.children);
+  // console.log(modelData.products[0].name);
+}
+// モデル切り替え
+let MDL = modelData.products[0];
+
+$('.modelicon').on('click', function () {
+  let ID = Number($(this).attr('id'));
+  MDL = modelData.products[ID];
+  setUrl();
+  // init(url);
+  console.log(`モデル${ID}`, url);
+});
+// デフォルトのモデルパス！
+let url = '3D/model/' + MDL.name + '_' + MDL.sm + '.glb';
+function setUrl() {
+  url = '3D/model/' + MDL.name + '_' + MDL.sm + '.glb';
+  init(url);
+}
+// 初期化
+window.addEventListener('load', init(url));
+const resetBtn = document.querySelector('#resetBtn');
+resetBtn.addEventListener('click', () => {
+  init(url);
+});
+
+// サイズ切り替え
+const modelNo = document.querySelector('#size-selector');
+
+modelNo.addEventListener('change', (e) => {
+  console.log('change', modelNo.value);
+  if (modelNo.value == 'sm') {
+    url = '3D/model/' + MDL.name + '_' + MDL.sm + '.glb';
+  } else if (modelNo.value == 'md') {
+    url = '3D/model/' + MDL.name + '_' + MDL.md + '.glb';
+  } else if (modelNo.value == 'lg') {
+    url = '3D/model/' + MDL.name + '_' + MDL.lg + '.glb';
+  }
+  init(url);
+});
+
+export default model;
---
+++ js/index.js
@@ -0,0 +1,91 @@
+//Login
+
+    var Panels = (function() {
+  
+        var panelLeft = document.querySelector('.panels__side--left');
+        var panelRight = document.querySelector('.panels__side--right');
+        var arrowLeft = document.querySelector('.loginarrow--left');
+        var arrowRight = document.querySelector('.loginarrow--right');
+
+        var loginBtn = document.getElementById('btn1a');
+        var signupBtn = document.getElementById('btn1b');
+      
+        var openLeft = function() {
+          panelLeft.classList.toggle('panels__side--left-active');
+          panelRight.classList.toggle('panels__side--right-hidden');
+        };
+      
+        var openRight1 = function() {
+          panelRight.classList.toggle('panels__side--right-active');
+          panelLeft.classList.toggle('panels__side--left-hidden');
+        };
+
+        var openRight2 = function() {
+          panelRight.classList.toggle('panels__side--right-active');
+          panelLeft.classList.toggle('panels__side--left-hidden');
+        };
+        
+        var bindActions = function() {
+          document.getElementById('btn2').addEventListener('click', openLeft, false);
+          arrowLeft.addEventListener('click', openLeft, false);
+
+          loginBtn.addEventListener('click', openRight1, false);
+          signupBtn.addEventListener('click', openRight2, false);
+          arrowRight.addEventListener('click', openRight2, false);
+        };
+        
+        var init = function() {
+          bindActions();
+        };
+        
+        return {
+          init: init
+        };
+       
+      }());
+      
+      Panels.init();
+
+      const $loginBtn = $("#btn1a");
+      const $loginCtn = $("#ctn1");
+
+      const $signupBtn = $("#btn1b");
+      const $signupCtn = $("#ctn2");
+
+      const $passBtn = $("#btn3");
+      const $passCtn = $("#ctn3");
+
+      const $customBtn = $("#btn2");
+
+      $loginBtn.on("click",login);
+      function login(){
+        $loginCtn.show();
+        $signupCtn.hide();
+        $passCtn.hide();
+      }
+
+      $signupBtn.on("click",signup);
+      function signup(){
+        $signupCtn.show();
+        $loginCtn.hide();
+        $passCtn.hide();
+      }
+
+      $passBtn.on("click",password);
+      function password(){
+        $passCtn.show();
+        $loginCtn.hide();
+        $signupCtn.hide();
+      }
+
+      $customBtn.on("click",redirect);
+
+      function redirect(){setTimeout(function (){
+        location.href="custom.html";
+      },1000);
+    }
+
+      
+      
+
+    
